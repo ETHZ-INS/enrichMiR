f0f8ee1e06c55008c90e2d61b3255c3c439c0df0
Pierre-Luc Germain <pl.germain@gmail.com>
2020-08-07 15:01:39 +0200

    minor updates on downstream browsing functions

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -30,7 +30,7 @@ setMethod("as.data.frame", "enrich.results", function (x, row.names = NULL, opti
 setMethod("show", "enrich.results", function(object){
     message("An `enrich.results` object with the following analyses:")
     print(lapply(object@res, FUN=function(x){
-        head(x[,grep("miRNA|pvalue|FDR|enrichment",colnames(x)),drop=FALSE])
+        head(x[,grep("set|pvalue|FDR|enrichment",colnames(x)),drop=FALSE])
     }))
 })
 
@@ -38,21 +38,22 @@ setMethod("show", "enrich.results", function(object){
 setMethod("summary", "enrich.results", function(object){
   message(paste("An `enrich.results` object with",length(object@res),"tests.\nAggregated top results:"))
   res <- getResults(object)
-  if(!("enrichment" %in% colnames(en))){
-    if("EN.combined.enrichment" %in% colnames(res)){
-      res$enrichment <- res$EN.combined.enrichment
-      res$overlap <- res$EN.combined.overlap
-      min.enr.thres <- -Inf
-    }else{
-      res$enrichment <- apply(abs(log2(as.matrix(res[,grep("enrichment"),drop=F])+0.05)),1,FUN=median)
-    }
+  if(!("enrichment" %in% colnames(res))){
+    res$enrichment <- apply(abs(log2(as.matrix(res[,grep("enrichment",colnames(res)),drop=F])+0.05)),1,FUN=median)
   }
-  res$medianP <- apply(res[,grep("pvalue",colnames(res)),drop=F],1,FUN=median)
-  head(res[order(res$medianP),intersect(c("miRNAs","expression","overlap","enrichment","medianP"),colnames(res))])
+  res$medianP <- apply(res[,grep("pvalue",colnames(res)),drop=F],1,na.rm=TRUE, FUN=median)
+  fields <- intersect(c("set","overlap","enrichment","medianP"),colnames(res))
+  if(length(f <- grep("FDR", colnames(res), value=TRUE))>0)
+    fields <- c(fields, f[1])
+  head(res[order(res$medianP),fields])
 })
 
 #' @export
 setMethod("$", "enrich.results", definition = function(x, name) {
   getResults(x,name)
-  }
-)
+})
+
+#' @export
+setMethod("names", "enrich.results", definition = function(x) {
+  names(x@res)
+})
--- R/misc.R
+++ R/misc.R
@@ -80,3 +80,42 @@ recapitalizeMiRs <- function(x){
   names(x) <- row.names(dea)
   x
 }
+
+
+#' dround
+#'
+#' Trim to a certain number of digits (similar to `format(...,digits=digits)`, 
+#' except that the output remains numeric)
+#'
+#' @param x A vector of numeric values, or a data.frame or similar
+#' @param digits The number of digits to keep
+#' @param roundGreaterThan1 Whether to trim also numbers greater than 1 (default FALSE)
+#'
+#' @return A object of same dimensions as `x`
+#' @export
+#'
+#' @examples
+#' dround( c(0.00002345, 554356, 12.56) )
+dround <- function(x, digits=3, roundGreaterThan1=FALSE){
+  if(!is.null(dim(x))){
+    for(i in seq_len(ncol(x))){
+      if(!is.integer(x[,i]) && is.numeric(x[,i])){
+        tryCatch({
+          x[,i] <- dround(as.numeric(x[,i]), digits, roundGreaterThan1)
+        }, error=function(e) warning(e))
+      }
+    }
+    return(x)
+  }  
+  if(roundGreaterThan1){
+    w <- 1:length(x)
+  }else{
+    w <- which(abs(x)<1)
+  }
+  if(length(w)==0) return(x)
+  e <- ceiling(-log10(abs(x[w])))
+  y <- x
+  y[w] <- round(10^e*x[w],digits-1)/10^e
+  y[x==0] <- 0
+  y
+}
