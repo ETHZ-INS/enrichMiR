07b5d5d8a69b588314bb9113312ad7cfd9c98eef
Soutschek  Michael <michasou@ethz.ch>
2020-10-08 11:49:06 +0200

    Update getGOgenes function

--- R/sublists.R
+++ R/sublists.R
@@ -3,22 +3,26 @@
 #' @param go_ids A vector of GO IDs.
 #' @param species A two-letter code indicating the species, such as `Mm` (default) or `Hs`.
 #' @param translate.ids Logical; whether to translate GO IDs into terms (default TRUE)
+#' @param ensembl_ids Logical; whether to translate Gene Symbols into Ensembl IDs
 #'
 #' @return
 #' @export
-getGOgenes <- function(go_ids, species="Mm", translate.ids=TRUE){
+getGOgenes <- function(go_ids, species="Mm", translate.ids=TRUE,ensembl_ids = FALSE){
   library(AnnotationDbi)
   if(!all(grepl("^GO:",go_ids)) && !is.null(names(go_ids))) go_ids <- names(go_ids)
   db <- paste0('org.',species,'.eg')
   library(package=paste0(db,'.db'), character.only = T)
-  eg <- mget(as.character(go_ids), get(paste0(db,'GO2ALLEGS')), ifnotfound=NA)
+  env <- get(paste0(db,'GO2ALLEGS'))
+  eg <- mget(as.character(go_ids),env , ifnotfound=NA)
   x <- lapply(eg, FUN=function(x){ 
     x <- x[which(!is.na(x))]
     if(length(x)==0) return(c())
-    unique(as.character(unlist(mget(as.character(x), get(paste0(db,'SYMBOL'))))))
+    if(ensembl_ids) {n <- 'ENSEMBL'}else{n <- 'SYMBOL'}
+    unique(as.character(unlist(mget(as.character(x), get(paste0(db,n))))))
   })
   x <- x[which(sapply(x,length)>0)]
   if(translate.ids) names(x) <- Term(names(x))
+  if(ensembl_ids){x <- lapply(x, function(y) y[!is.na(y)])}
   x
 }
 
