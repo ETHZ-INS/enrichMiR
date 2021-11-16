2e0fb33aacaffc10ca87a8760ddbb77abb1866cb
Soutschek  Michael <michasou@ethz.ch>
2021-11-16 12:38:46 +0100

    Preliminary fix install

--- R/enrichMiR.R
+++ R/enrichMiR.R
@@ -22,7 +22,7 @@
 #' @param background A character vector of background; ignored if `x` is not a
 #' character vector.
 #' @param tests Character vector of the tests to perform. See 
-#' \link{\code{enrichMiR::availableTests}} for the options.
+#' \code{\link{enrichMiR::availableTests}} for the options.
 #' @param sets.properties Any further information about the sets; this can 
 #' either be a data.frame (or DataFrame), with row.names corresponding to names
 #' of `sets` (or to alternative names), or a named vector (e.g. miRNA expression
@@ -165,8 +165,6 @@ testEnrichment <- function( x, sets, background=NULL, tests=NULL,
 #' @return an enrich.results object.
 #'
 #' @export
-#'
-#' @examples
 enrichMiR <- function( DEA, TS, miRNA.expression=NULL, families=NULL, cleanNames=FALSE, ...){
   if(is.null(families)){
     data("miR_families")
